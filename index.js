ddf4768d1a58032d0a73cfcbdfa43eb88822ee8e
Jay Vakil <61257499+Jdvakil@users.noreply.github.com>
2020-04-28 22:10:43 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function prof() {
+    let x = document.getElementById("pb");
+    if (x.innerHTML === "Professor") {
+        x.innerHTML = "Sergio Marquina";
+    } else {
+        x.innerHTML = "Professor";
+    }
+}
+function berlin() {
+    document.getElementById('berlin').innerHTML = "Andrés de Fonollosa";
+}
